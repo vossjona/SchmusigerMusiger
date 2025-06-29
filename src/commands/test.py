cd6ea717238dd14add3b17c9443e62988d29624f
Jonas Voss <jonas.voss@mgm-tp.com>
2025-06-29 11:08:53 +0200

    rename command files

--- src/commands/test.py
+++
@@ -1,9 +0,0 @@
-from discord.ext import commands
-
-async def test_cmd(ctx: commands.Context):
-    """Responds to !test with a placeholder message."""
-    await ctx.send("Test command is working!")
-
-
-def setup(bot: commands.Bot):
-    bot.add_command(commands.Command(test_cmd, name="test"))
